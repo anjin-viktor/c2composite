beb04f255116125f9add99df6ebd651073f0e2e0
Anjin Viktor <anjin_viktor@mail.ru>
2012-10-10 22:29:01 +0700

    Добавлены перечисления CType и CompositeType, функции преобразования в/из строк

--- trunk/src/util.c
+++ trunk/src/util.c
@@ -87,3 +87,257 @@ char *type_c_to_composite(char *dst, const char *src, size_t n)
 
 	return dst;
 }
+
+
+
+
+CType type_c_from_str(const char *str)
+{
+	if(strcmp(str, "signed char") == 0 || strcmp(str, "char") == 0)
+		return SIGNED_CHAR;
+	else if(strcmp(str, "unsigned char") == 0)
+		return UNSIGNED_CHAR;
+	else if(strcmp(str, "signed short") == 0 || strcmp(str, "short") == 0)
+		return SIGNED_SHORT;
+	else if(strcmp(str, "unsigned short") == 0)
+		return UNSIGNED_SHORT;
+	else if(strcmp(str, "signed int") == 0 || strcmp(str, "int") == 0)
+		return SIGNED_INT;
+	else if(strcmp(str, "unsigned int") == 0)
+		return UNSIGNED_INT;
+	else 
+		return C_NO_TYPE;
+}
+
+
+
+char *type_c_to_str(char *dst, CType type, size_t n)
+{
+	switch(type)
+	{
+		case SIGNED_CHAR:
+			if(dst == NULL)
+			{
+		 		n = strlen("signed char") + 1;
+				if((dst = (char *) malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "signed char", n);
+
+			break;
+
+		case UNSIGNED_CHAR:
+			if(dst == NULL)
+			{
+		 		n = strlen("unsigned char") + 1;
+				if((dst = (char *) malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "unsigned char", n);
+
+			break;
+
+		case SIGNED_SHORT:
+			if(dst == NULL)
+			{
+		 		n = strlen("signed short") + 1;
+				if((dst = (char *) malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "signed short", n);
+
+			break;
+
+		case UNSIGNED_SHORT:
+			if(dst == NULL)
+			{
+		 		n = strlen("unsigned short") + 1;
+				if((dst = (char *) malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "unsigned short", n);
+
+			break;
+
+		case SIGNED_INT:
+			if(dst == NULL)
+			{
+		 		n = strlen("signed int") + 1;
+				if((dst = (char *) malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "signed int", n);
+
+			break;
+
+		case UNSIGNED_INT:
+			if(dst == NULL)
+			{
+		 		n = strlen("unsigned int") + 1;
+				if((dst = (char *) malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "unsigned int", n);
+
+			break;
+
+		case C_NO_TYPE:
+			if(dst == NULL)
+			{
+		 		n = 1;
+				if((dst = (char *) malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			dst[0] = '\0';
+			break;
+	};
+
+	if(n > 0)
+		dst[n-1] = '\0';
+	else 
+		dst[0] = '\0';
+
+	return dst;
+}
+
+
+
+
+CompositeType type_composite_from_str(const char *str)
+{
+	CompositeType type = COMPOSITE_NO_TYPE;
+	if(strcmp(str, "schar") == 0)
+		type = SCHAR;
+	else if(strcmp(str, "uchar") == 0)
+		type = UCHAR;
+	else if(strcmp(str, "mod8") == 0)
+		type = MOD8;
+	else if(strcmp(str, "sshort") == 0)
+		type = SSHORT;
+	else if(strcmp(str, "ushort") == 0)
+		type = USHORT;
+	else if(strcmp(str, "mod16") == 0)
+		type = MOD16;
+	else if(strcmp(str, "sint") == 0)
+		type = SINT;
+	else if(strcmp(str, "uint") == 0)
+		type = UINT;
+	else if(strcmp(str, "mod32") == 0)
+		type = MOD32;
+
+	return type;
+}
+
+
+
+
+char *type_composite_to_str(char *dst, CompositeType type, size_t n)
+{
+	switch(type)
+	{
+		case UCHAR:
+			if(dst == NULL)
+			{
+				n = strlen("uchar") + 1;
+				if((dst = (char *)malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "uchar", n);
+			break;
+
+		case SCHAR:
+			if(dst == NULL)
+			{
+				n = strlen("schar") + 1;
+				if((dst = (char *)malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "schar", n);
+			break;
+
+		case MOD8:
+			if(dst == NULL)
+			{
+				n = strlen("mod8") + 1;
+				if((dst = (char *)malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "mod8", n);
+			break;
+
+		case USHORT:
+			if(dst == NULL)
+			{
+				n = strlen("ushort") + 1;
+				if((dst = (char *)malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "ushort", n);
+			break;
+
+		case SSHORT:
+			if(dst == NULL)
+			{
+				n = strlen("sshort") + 1;
+				if((dst = (char *)malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "sshort", n);
+			break;
+
+		case MOD16:
+			if(dst == NULL)
+			{
+				n = strlen("mod16") + 1;
+				if((dst = (char *)malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "mod16", n);
+			break;
+		case UINT:
+			if(dst == NULL)
+			{
+				n = strlen("uint") + 1;
+				if((dst = (char *)malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "uint", n);
+			break;
+
+		case SINT:
+			if(dst == NULL)
+			{
+				n = strlen("sint") + 1;
+				if((dst = (char *)malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "sint", n);
+			break;
+
+		case MOD32:
+			if(dst == NULL)
+			{
+				n = strlen("mod32") + 1;
+				if((dst = (char *)malloc(sizeof(char) * n)) == NULL)
+					return NULL;
+			}
+			strncpy(dst, "mod32", n);
+			break;
+
+		case COMPOSITE_NO_TYPE:
+			if(dst == NULL)
+			{
+				if((dst = (char *)malloc(sizeof(char))) == NULL)
+					return NULL;
+			}
+			dst[0] = '\0';
+			break;
+	};
+
+	if(n > 0)
+		dst[n-1] = '\0';
+	else
+		dst[0] = '\0';
+
+	return dst;
+}
--- trunk/unit_tests/util_test.c
+++ trunk/unit_tests/util_test.c
@@ -39,11 +39,99 @@ void type_c_to_composite_test(void)
 	CU_ASSERT_STRING_EQUAL(type_c_to_composite(str, "signed int", 3), "si");
 	CU_ASSERT_STRING_EQUAL(type_c_to_composite(str, "char", 2), "s");
 
-	CU_ASSERT_STRING_EQUAL(type_c_to_composite(NULL, "signed int", 0), "sint");
+	free(str);
+	str = NULL;
+	CU_ASSERT_STRING_EQUAL(type_c_to_composite(str, "signed int", 0), "sint");
 
 	free(str);
 }
 
+
+
+void type_c_from_str_test(void)
+{
+	CU_ASSERT_EQUAL(type_c_from_str("char"), SIGNED_CHAR);
+	CU_ASSERT_EQUAL(type_c_from_str("signed char"), SIGNED_CHAR);
+	CU_ASSERT_EQUAL(type_c_from_str("unsigned char"), UNSIGNED_CHAR);
+	CU_ASSERT_EQUAL(type_c_from_str("short"), SIGNED_SHORT);
+	CU_ASSERT_EQUAL(type_c_from_str("unsigned short"), UNSIGNED_SHORT);
+	CU_ASSERT_EQUAL(type_c_from_str("signed int"), SIGNED_INT);
+	CU_ASSERT_EQUAL(type_c_from_str("unsigned int"), UNSIGNED_INT);
+	CU_ASSERT_EQUAL(type_c_from_str("incorrect_type"), C_NO_TYPE);
+}
+
+
+
+void type_c_to_str_test(void)
+{
+	char *str;
+	CU_ASSERT_TRUE((int)(str = (char *)malloc(sizeof(char) * 64)));
+
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, SIGNED_CHAR, 64), "signed char");
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, UNSIGNED_CHAR, 64), "unsigned char");
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, SIGNED_SHORT, 64), "signed short");
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, UNSIGNED_SHORT, 64), "unsigned short");
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, SIGNED_INT, 64), "signed int");
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, UNSIGNED_INT, 64), "unsigned int");
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, C_NO_TYPE, 64), "");
+
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, SIGNED_CHAR, 5), "sign");
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, SIGNED_CHAR, 2), "s");
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, SIGNED_CHAR, 1), "");
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, SIGNED_CHAR, 0), "");
+
+	free(str);
+	str = NULL;
+
+	CU_ASSERT_STRING_EQUAL(type_c_to_str(str, SIGNED_INT, 0), "signed int");
+
+	free(str);
+}
+
+
+
+void type_composite_from_str_test(void)
+{
+	CU_ASSERT_EQUAL(type_composite_from_str("schar"), SCHAR);
+	CU_ASSERT_EQUAL(type_composite_from_str("uchar"), UCHAR);
+	CU_ASSERT_EQUAL(type_composite_from_str("sshort"), SSHORT);
+	CU_ASSERT_EQUAL(type_composite_from_str("ushort"), USHORT);
+	CU_ASSERT_EQUAL(type_composite_from_str("sint"), SINT);
+	CU_ASSERT_EQUAL(type_composite_from_str("uint"), UINT);
+	CU_ASSERT_EQUAL(type_composite_from_str("mod8"), MOD8);
+	CU_ASSERT_EQUAL(type_composite_from_str("mod16"), MOD16);
+	CU_ASSERT_EQUAL(type_composite_from_str("mod32"), MOD32);
+	CU_ASSERT_EQUAL(type_composite_from_str("incorrect"), COMPOSITE_NO_TYPE);
+}
+
+
+
+void type_composite_to_str_test(void)
+{
+	char *dst;
+	CU_ASSERT_TRUE((int)(dst = (char *)malloc(sizeof(char) * 16)));
+
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, SCHAR, 16), "schar");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, UCHAR, 16), "uchar");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, MOD8, 16), "mod8");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, SSHORT, 16), "sshort");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, USHORT, 16), "ushort");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, MOD16, 16), "mod16");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, SINT, 16), "sint");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, UINT, 16), "uint");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, MOD32, 16), "mod32");
+
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, MOD32, 5), "mod3");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, MOD32, 2), "m");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, MOD32, 0), "");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, MOD32, 1), "");
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, MOD32, 2), "m");
+
+	free(dst);
+	dst = NULL;
+	CU_ASSERT_STRING_EQUAL(type_composite_to_str(dst, MOD32, 16), "mod32");
+	free(dst);
+}
 
 
 int main(int argc, char **argv)
@@ -61,6 +149,31 @@ int main(int argc, char **argv)
 	}
 
 	if((CU_add_test(psuite, "test of type_c_to_composite()", type_c_to_composite_test) == NULL))
+	{
+		CU_cleanup_registry();
+		return CU_get_error();		
+	}
+
+	if((CU_add_test(psuite, "test of type_c_from_str()", type_c_from_str_test) == NULL))
+	{
+		CU_cleanup_registry();
+		return CU_get_error();		
+	}
+
+	if((CU_add_test(psuite, "test of type_c_to_str()", type_c_to_str_test) == NULL))
+	{
+		CU_cleanup_registry();
+		return CU_get_error();		
+	}
+
+
+	if((CU_add_test(psuite, "test of type_composite_from_str()", type_composite_from_str_test) == NULL))
+	{
+		CU_cleanup_registry();
+		return CU_get_error();		
+	}
+
+	if((CU_add_test(psuite, "test of type_composite_to_str()", type_composite_to_str_test) == NULL))
 	{
 		CU_cleanup_registry();
 		return CU_get_error();		
